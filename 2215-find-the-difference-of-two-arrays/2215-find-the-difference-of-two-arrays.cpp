6a2a90c9e5d379486b5927458604e776b8489592
SmarthGuyDev <98648192+SmarthGuyDev@users.noreply.github.com>
2022-03-28 11:32:47 +0530

    Time: 120 ms (44.44%), Space: 32.4 MB (22.22%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+	//unordered_set is implemented using a hash table where keys are hashed into indices of a hash table
+	// all operations take O(1) on average
+        unordered_set<int> n1;
+        unordered_set<int> n2;
+        for(int i=0;i<nums1.size();i++)
+            n1.insert(nums1[i]);
         
-        vector<vector<int>> res;
-        unordered_set<int> m1;
-        unordered_set<int> m2;
+        for(int i=0;i<nums2.size();i++)
+            n2.insert(nums2[i]);
         
-        for(int i = 0; i < nums1.size(); i++){
-            m1.insert(nums1[i]);
+        vector<int> ans1;
+		vector<vector<int>> ans;
+        for(int x:n1)
+        {
+            if(n2.find(x)==n2.end())
+                ans1.push_back(x);
         }
-         for(int i = 0; i < nums2.size(); i++){
-             m2.insert(nums2[i]);
+		ans.push_back(ans1);
+		ans1.clear();
+        for(int x:n2)
+        {
+            if(n1.find(x)==n1.end())
+                ans1.push_back(x);
         }
-        
-        vector<int> v1;
-        vector<int> v2;
-        
-        for(auto itr : m1){
-         for(auto it : m2){
-            if(m2.find(itr) == m2.end()){
-                v1.push_back(itr);
-                break;
-            }
-          }
-        }
-        for(auto itr : m2){
-          for(auto it : m1){
-            if(m1.find(itr) == m1.end()){
-                v2.push_back(itr);
-                break;
-            }
-          }
-        }
-        
-        res.push_back(v1);
-        res.push_back(v2);
-        
-        return res;
+        ans.push_back(ans1);
+        return ans;
     }
 };
