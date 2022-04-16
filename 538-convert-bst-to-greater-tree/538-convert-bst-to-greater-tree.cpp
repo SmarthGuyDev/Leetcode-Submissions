cd8924c4f29fe7e39c7d838b3a2f593883e2d224
SmarthGuyDev <98648192+SmarthGuyDev@users.noreply.github.com>
2022-04-17 03:29:15 +0530

    Time: 40 ms (80.56%), Space: 33.3 MB (93.89%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sum = 0;
+    TreeNode* convertBST(TreeNode* root) {
+        if (root) {
+            convertBST(root->right);
+            root->val += sum;
+            sum = root->val;
+            convertBST(root->left);
+        }
+        return root;
+    }
+};
