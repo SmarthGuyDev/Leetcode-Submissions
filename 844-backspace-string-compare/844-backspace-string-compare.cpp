0ed6dd7a412f40c80f2b881379c1f26da0ecaac8
SmarthGuyDev <98648192+SmarthGuyDev@users.noreply.github.com>
2022-05-02 04:55:22 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (39.09%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -5,18 +5,18 @@ public:
         stack<char> s1, s2;
         int n = s.size();
         int m = t.size();
-        for(int i = 0; i < n; i++){
-            if(s[i] == '#' && !s1.empty()){
+        for(auto i : s){
+            if(i == '#' && !s1.empty()){
                 s1.pop();
-            } else if(s[i] != '#') {
-                s1.push(s[i]);
+            } else if(i != '#') {
+                s1.push(i);
             }
         }
-         for(int i = 0; i < m; i++){
-            if(t[i] == '#' && !s2.empty()){
+         for(auto i : t){
+            if(i == '#' && !s2.empty()){
                 s2.pop();
-            } else if(t[i] != '#'){
-                s2.push(t[i]);
+            } else if(i != '#'){
+                s2.push(i);
             }
         }
         
