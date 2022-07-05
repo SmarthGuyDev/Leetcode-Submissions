213269e5700b7bb9cb75c6826286332f9de6b8fb
SmarthGuyDev <98648192+SmarthGuyDev@users.noreply.github.com>
2022-07-06 05:00:07 +0530

    Time: 75 ms (97.09%), Space: 33.8 MB (86.72%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -9,10 +9,10 @@ public:
             return 0;
         }
         
-        for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i - 1] + 1){
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i + 1] - nums[i] == 1){
                 count++;
-            } else if(nums[i] != nums[i - 1]) {
+            } else if(nums[i + 1] != nums[i]) {
                 mx = max(count, mx);
                 count = 1;
             }
