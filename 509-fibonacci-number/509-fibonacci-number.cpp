d9bfd556fdcb34782645e0dd1640ee5d34a5ba76
SmarthGuyDev <98648192+SmarthGuyDev@users.noreply.github.com>
2022-07-08 01:09:58 +0530

    Time: 6 ms (38.68%), Space: 6.2 MB (9.18%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //*TABULATION*//
+    //*Space Optimised*//
     int helper(int n, vector<int> &dp){
         int prev2 = 0;
         int prev = 1;
@@ -25,3 +25,94 @@ public:
         return ans;
     }
 };
+
+/*
+
+```
+class Solution {
+public:
+        
+    int fibHelper(int n, vector<int> &strg){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n]; 
+        }
+        
+        int fnm1 = fibHelper(n - 1, dp);
+        int fnm2 = fibHelper(n - 2, dp);
+        
+        int fn = fnm1 + fnm2;
+        
+        dp[n] = fn;
+        return fn;
+    }
+    
+    
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        int ans = fibHelper(n, dp);
+        return ans;
+    }
+};
+```
+```
+
+class Solution {
+public:
+    
+    //*MEMOISATION*/
+    
+/*
+    int helper(int n, vector<int> &dp){
+    
+        if(n == 0 || n == 1){
+            return n;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+        
+    } 
+    
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        
+        int ans = helper(n, dp);
+        return ans;
+    }
+};
+```
+```
+
+class Solution {
+public:
+    //*TABULATION*/
+/*
+    int helper(int n, vector<int> &dp){
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+    
+    
+    
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        int ans = helper(n, dp);
+        return ans;
+    }
+};
+```
+
+*/
